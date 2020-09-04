cf151a3d730c96a91db72c34bbcfe588b895db46
Sergy <decole@rambler.ru>
2020-09-04 23:26:27 +0400

    T70 планировщик задач

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,13 @@
 <?php
+
+use yii\mutex\MysqlMutex;
+use yii\queue\db\Queue;
+use yii\queue\LogBehavior;
+
 return [
+    'bootstrap' => [
+        'queue', // Компонент регистрирует свои консольные команды
+    ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -8,6 +16,14 @@ return [
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'queue' => [
+            'as log' => LogBehavior::class,
+            'class' => Queue::class,
+            'db' => 'db',
+            'tableName' => 'queue',
+            'channel' => 'default',
+            'mutex' => MysqlMutex::class,
         ],
     ],
 ];
---
+++ console/migrations/M161119140200Queue.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+use yii\db\Migration;
+
+/**
+ * Example of migration for queue message storage.
+ *
+ * @author Roman Zhuravlev <zhuravljov@gmail.com>
+ */
+class M161119140200Queue extends Migration
+{
+    public $tableName = 'queue';
+
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable($this->tableName, [
+            'id' => $this->primaryKey(),
+            'channel' => $this->string()->notNull(),
+            'job' => $this->binary()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'started_at' => $this->integer(),
+            'finished_at' => $this->integer(),
+        ], $tableOptions);
+
+        $this->createIndex('channel', $this->tableName, 'channel');
+        $this->createIndex('started_at', $this->tableName, 'started_at');
+    }
+
+    public function down()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
