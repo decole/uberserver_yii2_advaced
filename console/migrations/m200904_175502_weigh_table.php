47bb07df99e6e641eb195a1eb836a375f99f003a
Sergy <decole@rambler.ru>
2020-09-04 22:23:48 +0400

    T71 CRUD вес

---
+++ console/migrations/m200904_175502_weigh_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200904_175502_weigh_table
+ */
+class m200904_175502_weigh_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('decole_weight', [
+            'id' => $this->primaryKey(),
+            'weight' => $this->string()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('decole_weight');
+    }
+}
