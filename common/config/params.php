3ec4a8f6aa7cbb77303a5dd0efd0ef4e84b2b90e
Sergy <decole@rambler.ru>
2020-09-08 23:54:34 +0400

    оптимизация параметров

--- common/config/params.php
+++ common/config/params.php
@@ -5,4 +5,20 @@ return [
     'senderEmail' => 'noreply@uberserver.ru',
     'senderName' => 'Uberserver.ru mailer',
     'user.passwordResetTokenExpire' => 3600,
+
+    'MQTT_SERVER_IP'    => '192.168.1.5',
+    'MQTT_SERVER_PORT'  => 1883,
+
+    'SOCKS5_PROXY_TELEGRAM' => 'socks5://login:pass@123.123.212.123:1080',
+    'HTTPS_PROXY_TELEGRAM'  => 'https://login:pass@123.123.123.123:3128',
+
+    'TELEGRAM_BOT_TOKEN' => '1234567890:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    'TELEGRAM_HOOK_URL' => 'https://uberserver.ru/telegram/webhook',
+    'TELEGRAM_BOT_NAME' => 'UberserverBot',
+    'DECOLE_TELEGRAM_ID' => 1111111111,
+    'PANTERKA_TELEGRAM_ID' => 222222222,
+    'TG_DB_HOST' => '',
+    'TG_DB_USERNAME' => '',
+    'TG_DB_PASSWORD' => '',
+    'TG_DB_DATABASE' => '',
 ];
