6a01cfbd59d0ed5e83f4123c50278ba313a9a50e
Sergy <decole@rambler.ru>
2020-09-20 01:16:54 +0400

    T85 DRY

---
+++ common/services/mqtt/ValidateProcessor/BaseProcessor.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace common\services\mqtt\ValidateProcessor;
+
+use backend\jobs\TelegramNotifyJob;
+use Throwable;
+use yii\helpers\ArrayHelper;
+use Yii;
+
+class BaseProcessor implements DeviceInterface
+{
+    /**
+     * @var string
+     */
+    public $topicList;
+    /**
+     * @var string
+     */
+    public $topicModel;
+
+    /**
+     * @var
+     */
+    public $validateForm;
+
+    /**
+     * @var
+     */
+    public $validateModel;
+
+    /**
+     * @var \yii\caching\CacheInterface
+     */
+    protected $cache;
+
+    public function __construct($topicList, $topicsModel, $validateModel, $validateForm)
+    {
+        $this->cache = Yii::$app->cache;
+        $this->topicList = $topicList;
+        $this->topicModel = $topicsModel;
+        $this->validateModel = $validateModel;
+        $this->validateForm = $validateForm;
+        $this->createDataset();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * sensor_list - array current topics
+     * sensors     - models serialized in array
+     */
+    public function getTopics()
+    {
+        return $this->cache->getOrSet($this->topicList, function () {
+            $model = $this->validateModel::find()
+                ->orderBy(['id'=>SORT_ASC])
+                ->all();
+
+            return ArrayHelper::map($model, 'topic', 'name');
+        });
+    }
+
+    public function getSensorModel($topic)
+    {
+        $models = $this->cache->getOrSet($this->topicModel, function () {
+            return $this->validateModel::find()
+                ->orderBy(['id'=>SORT_ASC])
+                ->asArray()
+                ->all();
+        });
+
+        foreach ($models as $model) {
+            if ($model['topic'] == $topic) {
+               return $model;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     * @return void
+     */
+    public function createDataset()
+    {
+        $models = $this->cache->getOrSet($this->topicModel, function () {
+            return $this->validateModel::find()
+                ->orderBy(['id'=>SORT_ASC])
+                ->asArray()
+                ->all();
+        });
+
+        $this->cache->set($this->topicModel, $models);
+        $this->cache->set($this->topicList, self::getTopics());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deviceValidate($message)
+    {
+        try {
+            echo $message->topic . PHP_EOL;
+            $form = Yii::createObject($this->validateForm, [$message->topic, $message->payload, $this]);
+            if ($form->validate()) {
+                return;
+            }
+
+            $error = '';
+            foreach ($form->getErrors('payload') as $value) {
+                $error .= $value . "\n";
+            }
+
+            Yii::$app->queue->push(new TelegramNotifyJob([
+                'message' => $error,
+            ]));
+        } catch (Throwable $e) {
+            Yii::error($e->getMessage());
+        }
+    }
+
+    public function isSensor($topic)
+    {
+        return array_key_exists($topic, $this->getTopics()) ;
+    }
+}
--- common/services/mqtt/ValidateProcessor/LeakageProcessor.php
+++ common/services/mqtt/ValidateProcessor/LeakageProcessor.php
@@ -2,113 +2,13 @@
 
 namespace common\services\mqtt\ValidateProcessor;
 
-use backend\jobs\TelegramNotifyJob;
 use common\forms\LeakageValidateForm;
 use common\models\ModuleLeakage;
-use Throwable;
-use yii\helpers\ArrayHelper;
-use Yii;
 
-class LeakageProcessor implements DeviceInterface
+class LeakageProcessor extends BaseProcessor
 {
-    /**
-     * @var string
-     */
-    public $topicList;
-    /**
-     * @var string
-     */
-    public $topicModel;
-
-    protected $cache;
-
     public function __construct($topicList, $topicsModel)
     {
-        $this->cache = Yii::$app->cache;
-        $this->topicList = $topicList;
-        $this->topicModel = $topicsModel;
-        $this->createDataset();
-    }
-
-    /**
-     * @inheritDoc
-     *
-     * sensor_list - array current topics
-     * sensors     - models serialized in array
-     */
-    public function getTopics()
-    {
-        return $this->cache->getOrSet($this->topicList, function () {
-            $model = ModuleLeakage::find()
-                ->orderBy(['id'=>SORT_ASC])
-                ->all();
-
-            return ArrayHelper::map($model, 'topic', 'name');
-        });
-    }
-
-    public function getSensorModel($topic)
-    {
-        $models = $this->cache->getOrSet($this->topicModel, function () {
-            return ModuleLeakage::find()
-                ->orderBy(['id'=>SORT_ASC])
-                ->asArray()
-                ->all();
-        });
-
-        foreach ($models as $model) {
-            if ($model['topic'] == $topic) {
-               return $model;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @inheritDoc
-     * @return void
-     */
-    public function createDataset()
-    {
-        $model = ModuleLeakage::find()
-            ->orderBy(['id'=>SORT_ASC])
-            ->all();
-        $topics = ArrayHelper::map($model, 'topic', 'name');
-
-        $this->cache->set($this->topicModel, $model);
-        $this->cache->set($this->topicList, $topics);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function deviceValidate($message)
-    {
-        try {
-            /** @var LeakageValidateForm $form */
-            echo $message->topic . PHP_EOL;
-            $form = Yii::createObject(LeakageValidateForm::class, [$message->topic, $message->payload, $this]);
-            if ($form->validate()) {
-                return;
-            }
-
-            $error = '';
-            foreach ($form->getErrors('payload') as $value) {
-                $error .= $value . "\n";
-            }
-
-            echo ' ' . $error . PHP_EOL;
-            Yii::$app->queue->push(new TelegramNotifyJob([
-                'message' => $error,
-            ]));
-        } catch (Throwable $e) {
-            Yii::error($e->getMessage());
-        }
-    }
-
-    public function isSensor($topic)
-    {
-        return array_key_exists($topic, $this->getTopics()) ;
+        parent::__construct($topicList, $topicsModel, ModuleLeakage::class, LeakageValidateForm::class);
     }
 }
