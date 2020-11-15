3234d1f405defd92a2b46119f18bd8d36ab7b172
Galochkin Sergey <galochkin@insigne.ru>
2020-11-15 15:20:52 +0400

    T145 тест вывода дополнительного устройства

--- www/project/common/modules/yandexSmartHome/actions/DeviceAction.php
+++ www/project/common/modules/yandexSmartHome/actions/DeviceAction.php
@@ -7,22 +7,50 @@ class DeviceAction extends BaseAction
     public function run()
     {
         return [
-            "request_id" => $this->request_id,
-            "payload" => [
-                "user_id" => "decole2014",
-                "devices" => [
+            'request_id' => $this->request_id,
+            'payload' => [
+                'user_id' => 'decole2014',
+                'devices' => [
                     [
-                        "id" =>  '1',
-                        "name" =>  'switcher1',
-                        "type" =>  'devices.types.switch',
-                        "capabilities" => [
+                        'id' =>  '1',
+                        'name' =>  'switcher1',
+                        'type' =>  'devices.types.switch',
+                        'capabilities' => [
                             [
-                                "type" => "devices.capabilities.on_off",
-                                "retrievable" => true
+                                'type' => 'devices.capabilities.on_off',
+                                'retrievable' => true
                             ]
                         ],
-
-                    ]
+                    ],
+                    [
+                        'id' =>  '2',
+                        'name' =>  'margutemp',
+                        'type' =>  'devices.types.other',
+                        'capabilities' => [
+                            [
+                                'type' => 'devices.capabilities.on_off',
+                                'retrievable' => true
+                            ]
+                        ],
+                        'properties' => [
+                            [
+                                'type' => 'devices.properties.float',
+                                'retrievable' => true,
+                                'parameters' => [
+                                    'instance' => 'temperature',
+                                    'unit' => 'unit.celsius',
+                                ],
+                            ],
+                            [
+                                'type' => 'devices.properties.float',
+                                'retrievable' => true,
+                                'parameters' => [
+                                    'instance' => 'humidity',
+                                    'unit' => 'unit.percent',
+                                ],
+                            ],
+                        ],
+                    ],
                 ]
             ]
         ];
